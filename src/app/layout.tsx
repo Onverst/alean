6ce3f8ccea8013f1484dd31a1c73c05029bf7732
Aleksandr <aleksandr.ryzhkov96@gmail.com>
2026-07-07 12:53:05 +0300

    Roistat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,6 +82,16 @@ export default async function RootLayout({
                 '<div><img src="https://mc.yandex.ru/watch/98657632" style="position:absolute; left:-9999px;" alt="" /></div>',
             }}
           />
+          <Script id="roistat-counter" strategy="afterInteractive">
+            {`
+              (function(w, d, s, h, id) {
+                  w.roistatProjectId = id; w.roistatHost = h; w.roistatPage = d.location.href; w.roistatReferrer = d.referrer;
+                  var p = d.location.protocol == "https:" ? "https://" : "http://";
+                  var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+                  var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+              })(window, document, 'script', 'cloud.roistat.com', '353712acdb608e1b9aff97d37634f12c');
+            `}
+          </Script>
           <PopupProvider
             logo={popupLogo}
             image={popupImage}
