a42bfd93ce18059a7d5dc0b6496fd0cc9f1056ae
Aleksandr <aleksandr.ryzhkov96@gmail.com>
2026-07-07 12:19:40 +0300

    Яндекс метрика

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 import { getSiteOptions, fetchMediaById } from "@/lib/wordpress";
 import { Header } from "@/components/Header";
@@ -63,6 +64,24 @@ export default async function RootLayout({
       className={`${corsaGrotesk.variable} ${tenorSans.variable} ${ttLoveliesScript.variable}`}
     >
       <body>
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+              ym(98657632, 'init', {webvisor:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            `}
+          </Script>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html:
+                '<div><img src="https://mc.yandex.ru/watch/98657632" style="position:absolute; left:-9999px;" alt="" /></div>',
+            }}
+          />
           <PopupProvider
             logo={popupLogo}
             image={popupImage}
